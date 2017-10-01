9c7400bc1427260e9b8922f624a91342b558d1b3
Will Smith <minkcv@gmail.com>
2017-09-30 22:37:09 -0600

    generate buildings

--- city.js
+++ city.js
@@ -3,47 +3,60 @@ var width = threediv.clientWidth - 1;
 var height = threediv.clientHeight - 20;
 
 var scene = new THREE.Scene();
-var scale = 16;
-var camera = new THREE.OrthographicCamera(width / -scale, width / scale, height / scale, height / -scale, 0, 1000);
-camera.position.z = 50;
-camera.position.y = 25;
-camera.rotation.x -= 0.5;
+var scale = 3;
+var camera = new THREE.OrthographicCamera(width / -scale, width / scale, height / scale, height / -scale, 0, 4000);
+//camera.rotation.x -= Math.PI / 2
+camera.rotation.x = -0.5;
+// zoom out
+camera.position.z = 200;
+camera.position.y = 130;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 threediv.appendChild(renderer.domElement);
 
-var testgeom = new THREE.EdgesGeometry(new THREE.BoxGeometry(10, 10, 10));
-//var material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
 var material = new THREE.LineBasicMaterial({color: 0x00ff00});
-//var cube = new THREE.Mesh(testgeom, material);
-var cube = new THREE.LineSegments(testgeom, material);
-scene.add(cube);
 
-var points = [
-    new THREE.Vector3(0, 0, 0),
-    new THREE.Vector3(0, 0, 3),
-    new THREE.Vector3(0, 2, 3),
-    new THREE.Vector3(1, 0, 3),
-    new THREE.Vector3(1, 2, 3),
-    new THREE.Vector3(0, 2, 0),
-    new THREE.Vector3(2, 0, 0),
-    new THREE.Vector3(2, 0, 2),
-    new THREE.Vector3(2, 2, 2),
-    new THREE.Vector3(2, 2, 0)
-];
-var convexgeom = new THREE.ConvexGeometry(points);
-var shape = new THREE.LineSegments(new THREE.EdgesGeometry(convexgeom), new THREE.MeshBasicMaterial());
-scene.add(shape);
+// shorthand for adding vectors
+function addv(v1, v2) {
+    return new THREE.Vector3().addVectors(v1, v2);
+}
 
-var axis = new THREE.AxisHelper(5);
+// x, z, width, depth, height
+function generateBuilding(x, z, wi, de, he) {
+    var h = new THREE.Vector3(0, he, 0);
+    var p1 = new THREE.Vector3(x, 0, z);
+    var p2 = new THREE.Vector3(x, 0, z + de);
+    var p3 = new THREE.Vector3(x + wi, 0, z);
+    var p4 = new THREE.Vector3(x + wi, 0, z + de);
+    var points = [
+        p1, p2, p3, p4,
+        addv(p1, h), addv(p2, h), addv(p3, h), addv(p4, h)
+    ];
+    var convexgeom = new THREE.ConvexGeometry(points);
+    return new THREE.LineSegments(new THREE.EdgesGeometry(convexgeom), material);
+}
+
+var buildings = [];
+for (var i = -100; i < 100; i += 22) {
+    for (var j = -100; j < 100; j += 22) {
+        var r = Math.random() * 8 + 10;
+        var h = Math.random() * 30 + 10;
+        var x = Math.random() * 5 + i;
+        var z = Math.random() * 5 + j;
+        buildings.push(generateBuilding(x, z, r, r, h));
+    }
+}
+
+buildings.forEach(function(e){scene.add(e)});
+
+var axis = new THREE.AxisHelper(15);
 scene.add(axis);
 
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.y += 0.02;
     axis.rotation.y += 0.02;
-    shape.rotation.y += 0.02;
+    buildings.forEach(function(e){e.rotation.y += 0.01});
     renderer.render(scene, camera);
 }
 animate();
